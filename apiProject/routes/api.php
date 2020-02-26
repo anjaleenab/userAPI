6b9b4ba76a8e623adadb0f54745c25fa46002b59
anjaleena <anjaleenab@github.com>
2020-02-25 23:23:34 -0800

    completed the create User feature

--- apiProject/routes/api.php
+++ apiProject/routes/api.php
@@ -17,8 +17,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*
+* sets up a route that will use the createUser function
+* that is located in the UserController to respond to a POST request
+*/
+
 Route::post('/users/create', 'UserController@createUser');
 
+/*
+* sets up a route that will use the getUsers function
+* that is located in the UserController to respond to a GET request
+*/
+
 Route::get('/users', 'UserController@getUsers');
+
+/*
+* sets up a route that will use the updateUser function
+* that is located in the UserController to respond to a POST request
+*/
 
 Route::post('/users/update/{id}', 'UserController@updateUser');
